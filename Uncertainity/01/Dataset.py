3084267f00ab8d1912b243b9fdf6b956e86041e4
v-i-s-h <get_vichu@yahoo.com>
2019-01-06 06:36:27 +0530

    Generators for Dataset

--- Uncertainity/01/Dataset.py
+++ Uncertainity/01/Dataset.py
@@ -6,25 +6,43 @@ from tensorflow.keras.datasets import mnist
 
 class Dataset( object ):
     def get_train_batch( self, n, onehot = True ):
-        idx = np.random.choice( self.selTrainIdx, size = n , replace = False )
-        if onehot:
-            return self.trainX[idx], self.trainY1hot[idx]
-        else:
-            return self.trainX[idx], self.trainY[idx]
+        # Shuffle indices for random order every epoch
+        np.random.shuffle( self.selTrainIdx )
+        i = 0
+        while i < self.nTrainSamples: 
+            endIdx  = i+n if i < self.nTrainSamples else self.nTrainSamples
+            idx = self.selTrainIdx[i:endIdx]
+            if onehot:
+                yield (self.trainX[idx],self.trainY1hot[idx])
+            else:
+                yield (self.trainX[idx],self.trainY[idx])
+            i += n
     
     def get_test_batch( self, n, onehot = True ):
-        idx = np.random.choice( self.selTestIdx, size = n, replace = False )
-        if onehot:
-            return self.testX[idx], self.testY1hot[idx]
-        else:
-            return self.testX[idx], self.testY[idx]
+        # Shuffle indices for random order every epoch
+        np.random.shuffle( self.selTestIdx )
+        i = 0
+        while i < self.nTestSamples: 
+            endIdx  = i+n if i < self.nTestSamples else self.nTestSamples
+            idx = self.selTestIdx[i:endIdx]
+            if onehot:
+                yield (self.testX[idx],self.testY1hot[idx])
+            else:
+                yield (self.testX[idx],self.testY[idx])
+            i += n
 
     def get_other_batch( self, n, onehot = True ):
-        idx = np.random.choice( self.otherTrainIdx, size = n, replace = False )
-        if onehot:
-            return self.trainX[idx], self.trainY1hot[idx]
-        else:
-            return self.trainX[idx], self.trainY[idx]
+        # Shuffle indices for random order every epoch
+        np.random.shuffle( self.otherTrainIdx )
+        i = 0
+        while i < self.nOthSamples: 
+            endIdx  = i+n if i < self.nOthSamples else self.nOthSamples
+            idx = self.otherTrainIdx[i:endIdx]
+            if onehot:
+                yield (self.trainX[idx],self.trainY1hot[idx])
+            else:
+                yield (self.trainX[idx],self.trainY[idx])
+            i += n
     
 
 class MNISTDataset( Dataset ):
@@ -47,6 +65,10 @@ class MNISTDataset( Dataset ):
         self.selTestIdx     = np.concatenate( [testIndices[i] for i in self.selectedLabels] )
         self.otherTrainIdx  = np.concatenate( [trainIndices[i] for i in self.otherLabels] )
         self.otherTestIdx   = np.concatenate( [testIndices[i] for i in self.otherLabels] )
+
+        self.nTrainSamples  = self.selTrainIdx.shape[0]
+        self.nTestSamples   = self.selTestIdx.shape[0]
+        self.nOthSamples    = self.otherTrainIdx.shape[0]
 
         # Make one-hot
         self.idx2label  = { idx: label for (idx,label) in enumerate(self.selectedLabels) }
@@ -72,8 +94,23 @@ class MNISTDataset( Dataset ):
         print( "othTrainIdx : ", self.otherTrainIdx.shape )
         print( "othTestIdx  : ", self.otherTestIdx.shape )
         print( "label2idx   : ", self.label2idx )
-        print( "idx2label   : ", self.idx2label )    
+        print( "idx2label   : ", self.idx2label )  
+        print( "nTrainSample: ", self.nTrainSamples )
+        print( "nTestSamples: ", self.nTestSamples)
 
         
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    
     a = MNISTDataset( 6 )
+
+    trainGen = a.get_other_batch( 4*4, onehot = True )
+    (x,y) = next( trainGen )
+    
+    f, a = plt.subplots( 4, 4 )
+    for (i,ax1) in enumerate(a):
+        for (j,ax2) in enumerate(ax1):
+            ax2.imshow( np.squeeze(x[i*4+j,],axis=2) )
+    print( y )
+    plt.show()
+    
